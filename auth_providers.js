8adf24ee4c7898416dc6496ada79480bd8dd9c6d
yamennasr <126324384+yamennasr@users.noreply.github.com>
2026-02-19 12:00:55 +0200

    Update auth_providers.js

--- auth_providers.js
+++ auth_providers.js
@@ -1,6 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: "",
+  apiKey: "AIzaSyDTPTbVrKsObyk5bVhndrcV2IGXMzHjNII",
+  authDomain: "qiraahswipe.firebaseapp.com",
+  projectId: "qiraahswipe",
+  storageBucket: "qiraahswipe.firebasestorage.app",
+  messagingSenderId: "354349569766",
+  appId: "1:354349569766:web:cecdde944cdef4173f8d27"
   };
